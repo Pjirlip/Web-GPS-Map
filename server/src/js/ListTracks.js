05cc73c6ff7059ffe95f4d8a5dc844c4f511b7ee
Dominik Wirtz <dominik.wirtz@gmail.com>
2017-04-27 15:02:36 +0200

    add readtracks

--- server/src/js/ListTracks.js
+++ server/src/js/ListTracks.js
@@ -1,3 +1,34 @@
 /**
  * Created by dominik.wirtz on 27.04.17.
  */
+
+
+class ListTracks {
+    constructor() {
+        this.tracklist = [];
+        this.trackobjects = [];
+    }
+
+    readTrackList() {
+        const fs = require('fs');
+        var tmp = [];
+
+        files = (fs.readdir(dataFolder, (err, files) => {
+
+            var regex = new RegExp(".+\.json$");
+            if (!files) {
+                return;
+            }
+            files.forEach(file => {
+                if (!file.match(regex)) {
+                    return
+                }
+
+                var tmp = JSON.parse(fs.readFileSync(dataFolder + "/" + file)).features[0].properties.name;
+                tmp.push(tmp);
+            });
+        }));
+        this.trackobjects = tmp;
+    }
+
+}
