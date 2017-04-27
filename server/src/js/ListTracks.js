003913666f86ace220c923f1a1e6eb23843aecf1
Dominik Wirtz <dominik.wirtz@gmail.com>
2017-04-27 17:35:08 +0200

    catch JSON error

--- server/src/js/ListTracks.js
+++ server/src/js/ListTracks.js
@@ -11,19 +11,21 @@ module.exports = class ListTracks {
         const fs = require('fs');
 
         let files = (fs.readdir(this.datafolder, (err, files) => {
-            let regex = new RegExp(".+\.json$");
 
             files.forEach(file => {
-                if (file.match(regex)) {
+
                     let id = file.split(".",1).toString();
 
                     fs.readFile(this.datafolder + "/" + file, (err, data) => {
-                        this.trackobjects[id] = JSON.parse(data);
-                        this.tracklist[id] = this.trackobjects[id].features[0].properties.name;
+                        try {
+                            this.trackobjects[id] = JSON.parse(data);
+                            this.tracklist[id] = this.trackobjects[id].features[0].properties.name;
+                        }
+                        catch(e) {
+                            console.log(`${file} is no JSON file`);
+                        }
                     });
-
-
-                }
+                
             });
         }));
     }
