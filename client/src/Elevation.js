f48b0d802eb85318fdb1578270512007e95e681d
Dominik Wirtz <dominik.wirtz@gmail.com>
2017-05-12 13:16:41 +0200

    berechnung MeterRauf und Meter Runter

--- client/src/Elevation.js
+++ client/src/Elevation.js
@@ -9,28 +9,30 @@ module.exports = class Elevation {
 		this.ctx = this.canvas.getContext("2d");
 		this.heights = [];
 		this.maxHeight = -99999;
-		this.minHeigth = 99999;
+		this.minHeight = 99999;
 		this.maxPoints = 0;
+		this.laenge = 0;
+		this.meterRauf = 0;
+		this.meterRunter = 0;
+		this.lastheight = -99999;
 	}
 
 	draw(trackURL)	{
 		this.promise = new Promise((resolve) => {
 			this.heights = [];
 			this.maxPoints = 0;
-			this.actualPoint = 0;
 			this.maxHeight = -99999;
 			this.minHeight = 99999;
+			this.lastheight = -99999;
+			this.laenge = 0;
+			this.meterRauf = 0;
+			this.meterRunter = 0;
 			$.getJSON(trackURL, (data) => {
 				data.features[0].geometry.coordinates.forEach((coord) => {
 					this.heights.push(coord[2]);
-
-					if (coord[2] > this.maxHeight)				{
-						this.maxHeight = coord[2];
-					}
-					if (coord[2] < this.minHeight)				{
-						this.minHeight = coord[2];
-					}
-
+					this.calcLength(coord[0], coord[1]);
+					this.calcRaufRunter(coord[2]);
+					this.setMinMax(coord[2]);
 					this.maxPoints++;
 				});
 
@@ -47,7 +49,7 @@ module.exports = class Elevation {
 			this.ctx.moveTo(0, this.canvas.height);
 
 			var point = 0.0;
-			this.heights.forEach((actualHeight) => {
+			this.heights.forEach(() => {
 				this.ctx.lineTo(point * this.canvas.width / this.maxPoints, this.calcHeight(this.heights[point]));
 				point++;
 			});
@@ -55,11 +57,43 @@ module.exports = class Elevation {
 			this.ctx.lineTo(this.canvas.width, this.canvas.height);
 			this.ctx.closePath();
 			this.ctx.fill();
+			console.log("HoeheMax:" + this.maxHeight);
+			console.log("HoeheMin:" + this.minHeight);
+			console.log("Rauf: " + this.meterRauf);
+			console.log("Runter: " + this.meterRunter);
+			console.log("Laenge: " + this.laenge);
 		});
 	}
 
 	calcHeight(pHeight)	{
 		return this.canvas.height - ((pHeight - this.minHeight) / (this.maxHeight - this.minHeight) * this.canvas.height);
+	}
+
+	calcLength(laenge, breite) {
+		console.log("test");
+	}
+
+	calcRaufRunter(hight) {
+		if (this.lastheight === -99999) {
+			this.lastheight = hight;
+		}
+		else if (hight > this.lastheight) {
+			this.meterRauf += hight - this.lastheight;
+			this.lastheight = hight;
+		}
+		else if (hight < this.lastheight) {
+			this.meterRunter += this.lastheight - hight;
+			this.lastheight = hight;
+		}
+	}
+
+	setMinMax(minmaxHoehe) {
+		if (minmaxHoehe > this.maxHeight)				{
+			this.maxHeight = minmaxHoehe;
+		}
+		if (minmaxHoehe < this.minHeight)				{
+			this.minHeight = minmaxHoehe;
+		}
 	}
 
 };
